13051293808c33d0dea4b7496c83a81ef89fca87
kreipikc <basmenov5kir@gmail.com>
2024-11-30 14:29:53 +0300

    small fix

--- app/common/routers/books_router.py
+++ app/common/routers/books_router.py
@@ -2,29 +2,29 @@ from fastapi import APIRouter
 from app.common.schemas.book_schemas import SBook
 from app.common.service.repository import BookRepository
 
-router = APIRouter(prefix="/book", tags=["book"])
+router = APIRouter(prefix="/books", tags=["Books 📚"])
 
-@router.get("")
+@router.get("", summary="Get all books")
 async def get_all():
     books = await BookRepository.db_get_all()
     return {"books": books}
 
-@router.post("")
+@router.post("", summary="Add new book")
 async def add_book(book: SBook):
     book_id = await BookRepository.db_add_one(book)
     return {"book_id": book_id}
 
-@router.get("/{id_book}")
+@router.get("/{id_book}", summary="Get a book by id")
 async def get_one(id_book: int):
     book = await BookRepository.db_get_one(id_book)
     return {"book": book}
 
-@router.put("/{id_book}")
+@router.put("/{id_book}", summary="Update a specific book")
 async def update_book(book: SBook, id_book: int):
     result = await BookRepository.db_update(book, id_book)
     return {"Success": result}
 
-@router.delete("/{id_book}")
+@router.delete("/{id_book}", summary="Delete a specific book")
 async def delete_book(id_book: int):
     result = await BookRepository.db_delete(id_book)
     return {"Success": result}
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from app.common.database.database import create_tables, delete_tables
-from app.common.routers.book_router import router as book_router
+from app.common.routers.books_router import router as books_router
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -13,4 +14,8 @@ async def lifespan(app: FastAPI):
     print("Выключение")
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(book_router)
+app.include_router(books_router)
+
+@app.get("/")
+async def go_to_docs():
+    return RedirectResponse(url="/docs", status_code=308)
